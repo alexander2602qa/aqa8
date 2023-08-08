c9ede221a2b826ec6a2fac45e2f318d2695ccc5c
Alexander <alex.albasov@gmail.com>
2023-08-08 15:24:31 +0700

    initial commit

---
+++ src/test/java/page/VerificationPage.java
@@ -0,0 +1,32 @@
+package page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+
+    private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("data-test-id=action-verity");
+    private final SelenideElement errorNotification = $("data-test-id=error-notification");
+
+    public VerificationPage() {
+        codeField.shouldBe(Condition.visible);
+    }
+
+    public void verifyErrorNotificationVisibility() {
+        errorNotification.shouldBe(Condition.visible);
+    }
+
+    public DashboardPage validVerify(String verificationCode) {
+        verify(verificationCode);
+        return new DashboardPage();
+    }
+
+    public void verify(String verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+    }
+
+}
---
+++ src/test/java/test/BankLoginTest.java
@@ -0,0 +1,62 @@
+package test;
+
+import data.DataHelper;
+import data.SQLHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+
+    @Test
+    void shouldSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    void shouldGetErrorNotificationIfDateNotIncludeInDB() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisibility();
+    }
+
+    @Test
+    void shouldGetErrorIfIncorrectVerificationCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisibility();
+    }
+
+    @Test
+    void shouldGetErrorIfLoginIsCorrectButPasswordIsIncorrect() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var correctLogin = DataHelper.getAuthInfoWithTestData().getLogin();
+        var incorrectPassword = DataHelper.generateRandomUser().getPassword();
+        loginPage.validLogin(new DataHelper.AuthInfo(correctLogin, incorrectPassword));
+        loginPage.verifyErrorNotificationVisibility();
+    }
+
+//    @Test
+//    void shouldBlockIfIncorrectPasswordInputed3Times() {
+//        var loginPage = open("http://localhost:9999", LoginPage.class);
+//        var authInfo = DataHelper.generateRandomUser();
+//        var verificationPage = loginPage.validLogin(authInfo);
+//    }
+
+}
